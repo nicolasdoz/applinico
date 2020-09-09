3d978041706de0861b438903122cc1059d592cdd
Nicolas Doz <userc@localhost.localdomain>
2020-09-09 11:32:09 +0200

    first commit

---
+++ nico-service/target/site/apidocs/member-search-index.js
@@ -0,0 +1,1 @@
+memberSearchIndex = [{"p":"nico.app","c":"App","l":"App()"},{"p":"nico.app","c":"App","l":"main(String[])","url":"main-java.lang.String:A-"}]
---
+++ nico-service/target/site/apidocs/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"p":"nico.app","l":"App"}]
